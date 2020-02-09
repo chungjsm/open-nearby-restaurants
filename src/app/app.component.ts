0a20d11ce737d23a8eef5e722bb51fd90813eb0a
Josh Kim <kjosh@vmware.com>
2020-02-08 19:46:12 -0800

    Add header and set up app layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'open-nearby-restaurants';
 
   restaurantListProviders: string[] = ['yelp', 'google-maps', 'trip-advisor'];
   withoutYelp: string[] = ['google-maps', 'trip-advisor'];
   filterOutYelp = false;
 
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+
+  }
+
+  toggleFilterOutYelp() {
+    this.filterOutYelp = !this.filterOutYelp;
+  }
 }
